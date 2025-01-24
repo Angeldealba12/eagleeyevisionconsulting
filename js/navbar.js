47104052b0bb49b272730a5ae3d162af89f5f07f
Angeldealba12 <angel.dealba.garcia@gmail.com>
2025-01-23 21:16:31 -0600

    Fixing navbar and responsiveness

--- js/navbar.js
+++ js/navbar.js
@@ -3,20 +3,3 @@ function myFunction() {
     
     x.classList.toggle('active');
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.querySelector('.navbar');
-    
-    window.addEventListener('scroll', function() {
-        // You can adjust this value based on when you want the change to happen
-        // This example uses the window height as the trigger point
-        const triggerHeight = window.innerHeight;
-        
-        if (window.scrollY > triggerHeight) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
-}); 
-
